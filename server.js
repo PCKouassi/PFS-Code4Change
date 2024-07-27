4c1cfcb6a2f14415053606b85c7f5c3de4141476
Peter Kouassi <peterckouassi@gmail.com>
2024-07-27 08:21:31 -0500

    revamp

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+app.listen(port, ()=> console.log(`Dolphin app listening on port ${port}!`))
