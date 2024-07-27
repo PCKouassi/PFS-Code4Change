f39aba55a81544fb76059d7f24d366a56be22a87
PCKouassi <94588463+PCKouassi@users.noreply.github.com>
2024-07-27 12:58:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function resetGame(direction) {
         y : boardHeight/2,
         width: ballWidth,
         height: ballHeight,
-        velocityX : direction,
-        velocityY : 2
+        velocityX : direction*ball.velocityX,
+        velocityY : ball.velocityY
     }
 }
