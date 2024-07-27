46c712ee5b12031f8856482358f51eae05ab609e
NateHCode <94586947+NateHCode@users.noreply.github.com>
2024-07-27 13:06:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function resetGame(direction) {
         y : boardHeight/2,
         width: ballWidth,
         height: ballHeight,
-        velocityX : velocityX,
-        velocityY : velocityY
+        velocityX : direction * ball.velocityX,
+        velocityY : ball.velocityY
     }
 }
